41cfda85dd85064d9818f03f9ed4c04fd7bf77a1
Ratan Rai Sur <ratan.r.sur@gmail.com>
2018-08-12 12:21:38 -0400

    bin: rename scripts to not have extensions

--- bin/home_monitor.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-xrandr --output eDP-1-1 --mode 3840x2160 --pos 1280x2880 --rotate normal
-xrandr --output DP-3 --scale 2x2 --mode 2560x1440 --pos 600x0 --rotate normal
-~/bin/wallpaper.sh
